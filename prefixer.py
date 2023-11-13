ecc29eb0b90a64e65973bd5df985a26ce426296f
soumildatta <soumildatta@gmail.com>
2023-11-13 12:43:01 -0600

    Implement function to check if expression is prefix

--- prefixer.py
+++ prefixer.py
@@ -2,6 +2,30 @@ class Node:
     def __init__(self, value):
         self.value = value
 
+def is_valid_prefix(expression):
+    def is_operator(c):
+        return c in '+-*/'
+
+    def is_operand(c):
+        return c.isdigit() or c.isalpha()
+    
+    tokens = expression.split()
+
+    if not tokens or not is_operator(tokens[0]):
+        return False
+
+    operand_count = 0
+
+    for token in reversed(tokens):
+        if is_operand(token):
+            operand_count += 1
+        elif is_operator(token):
+            if operand_count < 2:
+                return False
+            operand_count -= 1 
+
+    return operand_count == 1
+
 def evaluate_prefix(expression):
     stack = []
     tokens = expression.split()
@@ -29,6 +53,7 @@ def evaluate_prefix(expression):
 
     return stack.pop()
 
-# Testing
-expression = "- + 2 * 3 5 8"
-print(evaluate_prefix(expression))
+if __name__ == '__main__':
+    expression = input('Enter prefix expression: ')
+
+    print(evaluate_prefix(expression))
