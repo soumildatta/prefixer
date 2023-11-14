844a7989970f1944c631248cccb09de416f05afc
soumildatta <soumildatta@gmail.com>
2023-11-13 22:59:34 -0600

    Implement infix and postfix notation output

--- test.py
+++ test.py
@@ -1,17 +1,16 @@
-# class to represent a node in a tree
+# Class to represent a node in a tree
 class Node:
     def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
 
-# functin to create a tree from a list of tokens
 def create_tree(tokens):
     if not tokens:
         return None
 
     token = tokens.pop(0)
-    # print(token, tokens)
+    print(token, tokens)
     node = Node(token)
 
     if token in '+-*/':
@@ -25,20 +24,17 @@ def to_infix(node):
     if node is None:
         return ""
 
-    # return the node if it is an operand/leaf
+    # If the node is an operand, return it as is
     if not node.left and not node.right:
-        print(node.val)
         return str(node.val)
 
-    # recursively call for left and right subtrees
+    # Recursively convert left and right subtrees to infix notation
     left_infix = to_infix(node.left)
-    # print(left_infix)
     right_infix = to_infix(node.right)
 
-    # in order traversal -- left, parent, right for the subtrees
-    return f"{left_infix} left {node.val} right {right_infix}"
+    # in order traversal -- left, parent, right
+    return f"({left_infix} {node.val} {right_infix})"
 
-# post order traversal
 def to_postfix(node):
     if node is None:
         return ""
@@ -47,16 +43,13 @@ def to_postfix(node):
     if not node.left and not node.right:
         return str(node.val)
 
-    # recursively call for left and right subtrees
+    # Recursively convert left and right subtrees to postfix notation
     left_postfix = to_postfix(node.left)
     right_postfix = to_postfix(node.right)
 
-    # post order traversal -- left, right, parent for the subtrees
+    # post order traversal -- left, right, parent
     return f"{left_postfix} {right_postfix} {node.val}"
 
-# function to get the result of the expression
-
-# function to print the tree
 def print_tree(node, indent="", position="root"):
     if node:
         print(indent + position + node.val)
@@ -65,13 +58,9 @@ def print_tree(node, indent="", position="root"):
         print_tree(node.right, indent, "R--- ")
 
 if __name__ == '__main__':
-    # read the expression from a file
-    filename = str(input("Enter the filename: "))
-    with open(filename, 'r') as file:
-        expression = file.read()
-
+    expression = "- + 2 * 3 5 9"
     tokens = expression.split()
     expression_tree = create_tree(tokens)
-    # print_tree(expression_tree)
+    print_tree(expression_tree)
     print("Infix Notation:", to_infix(expression_tree))
     print("Postfix Notation:", to_postfix(expression_tree))
