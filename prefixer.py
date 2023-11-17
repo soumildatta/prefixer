6c491d881f7da1f6509ceb6b7f7ce20ce35e5cca
soumildatta <soumildatta@gmail.com>
2023-11-17 11:47:09 -0600

    Fix is_valid and obtain result from postfix

--- prefixer.py
+++ prefixer.py
@@ -36,23 +36,35 @@ def to_infix(node):
     right_infix = to_infix(node.right)
 
     # in order traversal -- left, parent, right for the subtrees
-    return f"{left_infix} {node.val} {right_infix}"
+    return f"({left_infix} {node.val} {right_infix})"
 
-# post order traversal
+# post order traversal and finding result
 def to_postfix(node):
     if node is None:
         return ""
 
     # If the node is an operand, return it as is
     if not node.left and not node.right:
-        return str(node.val)
+        return str(node.val), int(node.val)
 
     # recursively call for left and right subtrees
-    left_postfix = to_postfix(node.left)
-    right_postfix = to_postfix(node.right)
+    left_postfix, left_result = to_postfix(node.left)
+    right_postfix, right_result = to_postfix(node.right)
 
     # post order traversal -- left, right, parent for the subtrees
-    return f"{left_postfix} {right_postfix} {node.val}"
+    expression = f"{left_postfix} {right_postfix} {node.val}"
+
+    # Need to calcualte the result
+    if node.val == '+':
+        result = left_result + right_result
+    elif node.val == '-':
+        result = left_result - right_result
+    elif node.val == '*':
+        result = left_result * right_result
+    elif node.val == '/':
+        result = left_result / right_result
+
+    return expression, result
 
 # function to get the result of the expression
 def evaluate(infix_str):
@@ -60,8 +72,8 @@ def evaluate(infix_str):
 
 # TODO: Finish this function
 def is_valid(tokens):
-    if tokens[0] in '+-*/':
-        return False
+    if tokens[0] in '+-/*':
+        return True
 
 # function to print the tree
 def print_tree(node, indent="", position="root"):
@@ -70,19 +82,6 @@ def print_tree(node, indent="", position="root"):
         indent += "     " if position == "root" else "|  "
         print_tree(node.left, indent, "L--- ")
         print_tree(node.right, indent, "R--- ")
-
-# if __name__ == '__main__':
-#     # read the expression from a file
-#     filename = str(input("Enter the filename: "))
-#     with open(filename, 'r') as file:
-#         expression = file.read()
-
-#     tokens = expression.split()
-#     expression_tree = create_tree(tokens)
-#     # print_tree(expression_tree)
-#     print("Infix Notation:", to_infix(expression_tree))
-#     print("Postfix Notation:", to_postfix(expression_tree))
-#     # print(evaluate(to_infix(expression_tree)))
 
 if __name__ == '__main__':
     filename = str(input("Enter the filename: "))
@@ -102,8 +101,10 @@ if __name__ == '__main__':
 
             # output the infix and postfix notations
             print("Infix Notation:", to_infix(expression_tree))
-            print("Postfix Notation:", to_postfix(expression_tree))
-            print("Evaluation Result:", evaluate(to_infix(expression_tree)))
+
+            postfix_expr, result = to_postfix(expression_tree)
+            print("Postfix Notation:", postfix_expr)
+            print("Evaluation Result:", result)
 
     except FileNotFoundError:
         print(f"File '{filename}' not found.")
